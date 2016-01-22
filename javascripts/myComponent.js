06b494c30c203f0e8c2273e116a6f7d2ca91ee19
zhengwei <zhengweiSE@gmail.com>
2016-01-22 17:28:06 +0800

    no message

--- javascripts/myComponent.js
+++ javascripts/myComponent.js
@@ -1,32 +1,33 @@
-var ClickTitleChild=React.createClass({
-	render:function(){
-		return (<span class="octicon octicon-link">{this.props.theHrefText}</span>);
-	}
-});
-var ClickTitle = React.createClass({
-	render:function(){
-		return (<a id="unclosablegithubio" class="anchor" href={this.props.thehref} aria-hidden="true">
-			<ClickTitleChild theHrefText={this.props.theHrefText}/></a>);
-	}
-});
-var Title = React.createClass({
-	render:function(){
-		return (<h1>
-			<ClickTitle theHrefText={this.props.theHrefText} thehref={this.props.thehref}/>
-			{this.props.titlsText}
-			</h1>);
-	}
-});
-var Content = React.createClass({
-	render:function(){
-		return (<p>{this.props.contentText}</p>);
-	}
-});
-var Section = React.createClass({
-	render:function() {
-		return (<section id="main_content" class="inner">
-			<Title  theHrefText={this.props.theHrefText} thehref={this.props.thehref} titlsText={this.props.titlsText}/>
-			<Content contentText={this.props.contentText}/>
-			</section>);
-	}
-});
+
+  var ClickTitleChild=React.createClass({
+    render:function(){
+      return (<span className="octicon octicon-link">{this.props.theHrefText}</span>);
+    }
+  });
+  var ClickTitle = React.createClass({
+    render:function(){
+      return (<a id="unclosablegithubio" className="anchor" href={this.props.thehref} aria-hidden="true">
+        <ClickTitleChild theHrefText={this.props.theHrefText}/></a>);
+    }
+  });
+  var Title = React.createClass({
+    render:function(){
+      return (<h1>
+        <ClickTitle theHrefText={this.props.theHrefText} thehref={this.props.thehref}/>
+        {this.props.titlsText}
+        </h1>);
+    }
+  });
+  var Content = React.createClass({
+    render:function(){
+      return (<p>{this.props.contentText}</p>);
+    }
+  });
+  var Section = React.createClass({
+    render:function() {
+      return (<section id="main_content" className="inner">
+        <Title  theHrefText={this.props.theHrefText} thehref={this.props.thehref} titlsText={this.props.titlsText}/>
+        <Content contentText={this.props.contentText}/>
+        </section>);
+    }
+  });
