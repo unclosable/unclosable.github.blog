14e8e530745b2b883bf71e4c9a5744eb3cadf861
zhengwei <zhengweiSE@gmail.com>
2016-01-29 16:01:44 +0800

    现在你可以在这个页面看时间了，一颗赛艇

---
+++ javascripts/timer.jsx
@@ -0,0 +1,40 @@
+var Timer = React.createClass({
+  //获取初始化状态
+  getInitialState: function() {
+    console.log('初始化');
+    return this.getTimeNow();
+  },
+  //一个操作函数
+  tick: function() {
+    this.setState(this.getTimeNow());
+  },
+  //组件安装
+  componentDidMount: function() {
+    console.log('安装函数');
+    //setInterval() 方法可按照指定的周期（以毫秒计）来调用函数或计算表达式
+    this.interval = setInterval(this.tick, 1000);
+  },
+  //组件卸载
+  componentWillUnmount: function() {
+    console.log('卸载函数');
+    clearInterval(this.interval);
+  },
+  getTimeNow:function(){
+    var date=new Date();
+    return {
+      year:date.getYear()+1900,
+      month:date.getMonth()+1,
+      day:date.getDate(),
+      hours:date.getHours(),
+      minutes:date.getMinutes(),
+      seconds:date.getSeconds()
+    };
+  },
+  render: function() {
+    return (
+      <div>{this.state.year}-{this.state.month}-{this.state.day} {this.state.hours}:{this.state.minutes}:{this.state.seconds}</div>
+      );
+  }
+});
+
+ReactDOM.render(<Timer />, document.getElementById('forkme_banner'));
