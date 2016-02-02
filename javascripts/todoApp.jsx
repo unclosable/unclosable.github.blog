b9fd8f23f68d7c9ac18afe92e7e0a6eb549658ac
zhengwei <zhengweiSE@gmail.com>
2016-02-02 16:47:05 +0800

    更新了一个留言板的功能(☆_☆)

--- javascripts/todoApp.jsx
+++ javascripts/todoApp.jsx
@@ -1,37 +1,57 @@
 var TodoList = React.createClass({
   render: function() {
     var createItem = function(item) {
-      return <li key={item.id}>{item.text}</li>;
+      return  <li key={item.id} className="message">
+                <p className="itemTitle"> {item.editor} <font className="said">said</font></p>
+                <p className="itemContent">{item.content}</p>
+              </li>;
     };
     return <ul>{this.props.items.map(createItem)}</ul>;
   }
 });
 var TodoApp = React.createClass({
   getInitialState: function() {
-    return {items: [], text: ''};
+    return {items: this.props.items, editor: '',content:''};
   },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
+  onEditorChange: function(e) {
+    this.setState({editor:e.target.value});
+  },
+  onContentChange:function(e){
+    this.setState({content:e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+    var nextItems = this.state.items.concat([{editor: this.state.editor,content:this.state.content, id: Date.now()}]);
     var nextText = '';
-    this.setState({items: nextItems, text: nextText});
+    this.setState({items: nextItems, editor: nextText,content:nextText});
   },
   render: function() {
     return (
       <div>
-        <h3>留言板</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <textarea />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
+      <h3 className="messageTitle">留言板</h3>
+      <TodoList items={this.state.items} />
+      <form className="messageForm" onSubmit={this.handleSubmit}>
+        <table>
+          <tr>
+            <td className="firstInput">留言ID:</td>
+            <td><input onChange={this.onEditorChange} value={this.state.editor} className="titleInput"/></td>
+          </tr>
+          <tr>
+            <td>留言内容:</td>
+            <td><textarea onChange={this.onContentChange} value={this.state.content} className="contentInput"/></td>
+          </tr>
+          <tr>
+            <td colSpan="2" className="buttonTd"><button>{'添加评论'}</button></td>
+          </tr>
+        </table>
+      
+      </form>
       </div>
-    );
+      );
   }
 });
 
-//ReactDOM.render(<TodoApp />, $("#mountNode")[0]);
+    $.get("data/message.json", function(result) {
+      console.log(result);
+        ReactDOM.render(<TodoApp items={result}/>, $("#mountNode")[0]);
+    });
